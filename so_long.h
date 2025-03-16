fbb0dfc8d8967c3951e8a73393168154ecdcca37
Guilherme Urbano <guilhermeurbano@MacBookPro.lan>
2025-03-16 15:34:42 +0000

    :tada:

--- so_long.h
+++ so_long.h
@@ -6,14 +6,13 @@
 /*   By: guisanto <guisanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 14:58:20 by guisanto          #+#    #+#             */
-/*   Updated: 2025/03/12 15:52:28 by guisanto         ###   ########.fr       */
+/*   Updated: 2025/03/16 15:33:28 by guisanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include <mlx.h>
 # include <stdlib.h>
 # include <stdio.h>
 # include "libft/libft.h"
