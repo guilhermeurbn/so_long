3f8f31060d12577470108c8e0015d2757bd513e4
Guilherme Santos Urbano <guisanto@c1r1s3.42lisboa.com>
2025-04-16 19:04:26 +0100

    :tada:

--- sources/player_update.c
+++ sources/player_update.c
@@ -5,10 +5,134 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: guisanto <guisanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/31 16:19:28 by guisanto          #+#    #+#             */
-/*   Updated: 2025/04/16 17:23:02 by guisanto         ###   ########.fr       */
+/*   Created: 2021/10/01 18:51:06 by igvaz-fe          #+#    #+#             */
+/*   Updated: 2025/04/16 18:54:49 by guisanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+static void	player_update_image(char key, t_game *game)
+{
+	mlx_destroy_image(game->mlx, game->img_player);
+	if (key == 'w')
+		game->img_player = mlx_xpm_file_to_image
+			(game->mlx, "images/iron_man1.xpm", &game->img_w, &game->img_h);
+	else if (key == 's')
+		game->img_player = mlx_xpm_file_to_image
+			(game->mlx, "images/iron_man2.xpm", &game->img_w, &game->img_h);
+	else if (key == 'd')
+		game->img_player = mlx_xpm_file_to_image
+			(game->mlx, "images/iron_man1.xpm", &game->img_w, &game->img_h);
+	else if (key == 'a')
+		game->img_player = mlx_xpm_file_to_image
+			(game->mlx, "images/iron_man2.xpm", &game->img_w, &game->img_h);
+}
+
+void	player_w(t_game *game)
+{
+	player_update_image('w', game);
+	if (game->map[game->y_player][game->x_player] == 'E'
+			&& game->n_colect == 0)
+	{
+		mlx_clear_window(game->mlx, game->win);
+		game->map[game->y_player + 1][game->x_player] = '0';
+		game->moves++;
+		game->endgame = 1;
+		map_draw(game);
+	}
+	else if (game->map[game->y_player][game->x_player] == '1'
+			|| game->map[game->y_player][game->x_player] == 'E')
+		game->y_player += 1;
+	else
+	{
+		mlx_clear_window(game->mlx, game->win);
+		if (game->map[game->y_player][game->x_player] == 'C')
+			game->n_colect -= 1;
+		game->map[game->y_player][game->x_player] = 'P';
+		game->map[game->y_player + 1][game->x_player] = '0';
+		game->moves++;
+		map_draw(game);
+	}
+}
+
+void	player_s(t_game *game)
+{
+	player_update_image('s', game);
+	if (game->map[game->y_player][game->x_player] == 'E'
+			&& game->n_colect == 0)
+	{
+		mlx_clear_window(game->mlx, game->win);
+		game->map[game->y_player - 1][game->x_player] = '0';
+		game->moves++;
+		game->endgame = 1;
+		map_draw(game);
+	}
+	else if (game->map[game->y_player][game->x_player] == '1'
+			|| game->map[game->y_player][game->x_player] == 'E')
+		game->y_player -= 1;
+	else
+	{
+		mlx_clear_window(game->mlx, game->win);
+		if (game->map[game->y_player][game->x_player] == 'C')
+			game->n_colect -= 1;
+		game->map[game->y_player][game->x_player] = 'P';
+		game->map[game->y_player - 1][game->x_player] = '0';
+		game->moves++;
+		map_draw(game);
+	}
+}
+
+void	player_d(t_game *game)
+{
+	player_update_image('d', game);
+	if (game->map[game->y_player][game->x_player] == 'E'
+			&& game->n_colect == 0)
+	{
+		mlx_clear_window(game->mlx, game->win);
+		game->map[game->y_player][game->x_player - 1] = '0';
+		game->moves++;
+		game->endgame = 1;
+		map_draw(game);
+	}
+	else if (game->map[game->y_player][game->x_player] == '1'
+			|| game->map[game->y_player][game->x_player] == 'E')
+		game->x_player -= 1;
+	else
+	{
+		mlx_clear_window(game->mlx, game->win);
+		if (game->map[game->y_player][game->x_player] == 'C')
+			game->n_colect -= 1;
+		game->map[game->y_player][game->x_player] = 'P';
+		game->map[game->y_player][game->x_player - 1] = '0';
+		game->moves++;
+		map_draw(game);
+	}
+}
+
+void	player_a(t_game *game)
+{
+	player_update_image('a', game);
+	if (game->map[game->y_player][game->x_player] == 'E'
+			&& game->n_colect == 0)
+	{
+		mlx_clear_window(game->mlx, game->win);
+		game->map[game->y_player][game->x_player + 1] = '0';
+		game->moves++;
+		game->endgame = 1;
+		map_draw(game);
+	}
+	else if (game->map[game->y_player][game->x_player] == '1'
+			|| game->map[game->y_player][game->x_player] == 'E')
+		game->x_player += 1;
+	else
+	{
+		mlx_clear_window(game->mlx, game->win);
+		if (game->map[game->y_player][game->x_player] == 'C')
+			game->n_colect -= 1;
+		game->map[game->y_player][game->x_player] = 'P';
+		game->map[game->y_player][game->x_player + 1] = '0';
+		game->moves++;
+		map_draw(game);
+	}
+}
