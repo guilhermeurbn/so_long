6c593ae45e2a583fbe4218b83a00bc7c09233340
Guilherme Urbano <guilhermeurbano@MacBook-Pro-de-Guilherme.local>
2025-04-21 13:07:30 +0100

    pronto

--- sources/draw.c
+++ sources/draw.c
@@ -6,58 +6,59 @@
 /*   By: guisanto <guisanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 16:02:42 by guisanto          #+#    #+#             */
-/*   Updated: 2025/04/17 18:51:58 by guisanto         ###   ########.fr       */
+/*   Updated: 2025/04/21 13:06:57 by guisanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void    img_draw(t_game *game, void *image, int x, int y)
+void	img_draw(t_game *game, void *image, int x, int y)
 {
-    mlx_put_image_to_window
+	mlx_put_image_to_window
 		(game->mlx, game->win, image, x * 32, y * 32);
 }
-static void    player_draw(t_game *game, void *image, int x, int y)
+
+static void	player_draw(t_game *game, void *image, int x, int y)
 {
-    game->x_player = x;
-    game->y_player = y;
-    img_draw(game, image, x, y);
+	game->x_player = x;
+	game->y_player = y;
+	img_draw(game, image, x, y);
 }
-static void    exit_draw(t_game *game, int x, int y)
+
+static void	exit_draw(t_game *game, int x, int y)
 {
-    if (game->n_colect == 0)
-    {
-        mlx_destroy_image(game->mlx, game->img_exit);
-        game->img_exit = mlx_xpm_file_to_image
+	if (game->n_colect == 0)
+	{
+		mlx_destroy_image(game->mlx, game->img_exit);
+		game->img_exit = mlx_xpm_file_to_image
 			(game->mlx, "images/exit_open.xpm", &game->img_w, &game->img_h);
-    }
-    img_draw(game, game->img_exit, x, y);
+	}
+	img_draw(game, game->img_exit, x, y);
 }
 
-int map_draw(t_game *game)
+int	map_draw(t_game *game)
 {
-    int x;
-    int y;
+	int	y;
+	int	x;
 
-    y = 0;
-    while (game->map[y])
-    {
-        x = 0;
-        while (game->map[y][x])
-        {
-            if (game->map[y][x] == '1')
-            	img_draw(game, game->img_wall, x, y);
-            else if (game->map[y][x] == '0')
-            	img_draw(game, game->img_backg, x, y);
-            else if (game->map[y][x] == 'P')
-            	player_draw(game, game->img_player,x, y);
-            else if (game->map[y][x] == 'C')
-            	img_draw(game, game->img_colect, x, y);
-            else if (game->map[y][x] == 'E')
-            	exit_draw(game, x, y);
-            x++;
-        }
-        y++;
-    }
-    return (0);
+	y = 0;
+	while (game->map[y])
+	{
+		x = 0;
+		while (game->map[y][x])
+		{
+			img_draw(game, game->img_backg, x, y);
+			if (game->map[y][x] == '1')
+				img_draw(game, game->img_wall, x, y);
+			else if (game->map[y][x] == 'P')
+				player_draw(game, game->img_player, x, y);
+			else if (game->map[y][x] == 'C')
+				img_draw(game, game->img_colect, x, y);
+			else if (game->map[y][x] == 'E')
+				exit_draw(game, x, y);
+			x++;
+		}
+		y++;
+	}
+	return (0);
 }
