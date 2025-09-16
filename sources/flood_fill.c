5870a11f8b6642988e5f057368db0f72ebb57462
Guilherme Santos Urbano <guisanto@c2r7s2.42lisboa.com>
2025-09-16 11:03:20 +0100

    vvai

--- sources/flood_fill.c
+++ sources/flood_fill.c
@@ -12,104 +12,108 @@
 
 #include "so_long.h"
 
-void free_dup(char **dup, int i)
+void	free_dup(char **dup, int i)
 {
-    int j = 0;
-    while (j < i)
-    {
-        free(dup[j]);
-        j++;
-    }
-    free(dup);
+	int	j;
+
+	j = 0;
+	while (j < i)
+	{
+		free(dup[j]);
+		j++;
+	}
+	free(dup);
 }
 
-char **flood_fill(char **map, int x, int y)
+char	**flood_fill(char **map, int x, int y)
 {
-    if (y < 0 || !map[y] || x < 0 || x >= (int)ft_strlen(map[y]))
-        return 0;
-
-    if (map[y][x] == '1' || map[y][x] == 'V')
-        return 0;
-    map[y][x] = 'V';
-
-    flood_fill(map, x + 1, y);
-    flood_fill(map, x - 1, y);
-    flood_fill(map, x, y + 1);
-    flood_fill(map, x, y - 1);
-
-    return map;
+	if (y < 0 || !map[y] || x < 0 || x >= (int)ft_strlen(map[y]))
+		return (0);
+	if (map[y][x] == '1' || map[y][x] == 'V')
+		return (0);
+	map[y][x] = 'V';
+	flood_fill(map, x + 1, y);
+	flood_fill(map, x - 1, y);
+	flood_fill(map, x, y + 1);
+	flood_fill(map, x, y - 1);
+	return (map);
 }
 
-char **map_dup(char **map)
+char	**map_dup(char **map)
 {
-    int i = 0;
-    int len = 0;
-    char **dup;
+	int		i;
+	int		len;
+	char	**dup;
 
-    while (map[len])
-        len++;
-    dup = malloc(sizeof(char *) * (len + 1));
-    if (!dup)
-        return NULL;
-    while (i < len)
-    {
-        dup[i] = ft_strdup(map[i]);
-        if (!dup[i])
-        {
-            free_dup(dup, i);
-            return NULL;
-        }
-        i++;
-    }
-    dup[i] = NULL;
-    return dup;
+	i = 0;
+	len = 0;
+	while (map[len])
+		len++;
+	dup = malloc(sizeof(char *) * (len + 1));
+	if (!dup)
+		return (NULL);
+	while (i < len)
+	{
+		dup[i] = ft_strdup(map[i]);
+		if (!dup[i])
+		{
+			free_dup(dup, i);
+			return (NULL);
+		}
+		i++;
+	}
+	dup[i] = NULL;
+	return (dup);
 }
 
-int find_player(char **map, int *x, int *y)
+int	find_player(char **map, int *x, int *y)
 {
-    int i = 0;
-    int j;
-    while (map[i])
-    {
-        j = 0;
-        while (map[i][j])
-        {
-            if (map[i][j] == 'P')
-            {
-                *x = j;
-                *y = i;
-                return 1;
-            }
-            j++;
-        }
-        i++;
-    }
-    return 0;
+	int	i;
+	int	j;
+
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == 'P')
+			{
+				*x = j;
+				*y = i;
+				return (1);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
 
-int checker_E_C(t_game *game)
+int	checker_E_C(t_game *game)
 {
-    int i;
-    int j;
-    i = 0;
+	int		i;
+	int		j;
+	char	**map_dupp;
 
-    char **map_dupp = map_dup(game->map);
-    if (!map_dupp)
-        return 0;
-    if (!find_player(map_dupp, &game->x_player, &game->y_player))
-        return (free_map(map_dupp), 0);
-    flood_fill(map_dupp, game->x_player, game->y_player);
-    while (map_dupp[i])
-    {
-        j = 0;
-        while (map_dupp[i][j])
-        {
-            if (map_dupp[i][j] == 'E' || map_dupp[i][j] == 'C')
-                return (free_map(map_dupp), 0);
-            j++;
-        }
-        i++;
-    }
-    free_map(map_dupp);
-    return 1;
+	i = 0;
+	map_dupp = map_dup(game->map);
+	if (!map_dupp)
+		return (0);
+	if (!find_player(map_dupp, &game->x_player, &game->y_player))
+		return (free_map(map_dupp), 0);
+	flood_fill(map_dupp, game->x_player, game->y_player);
+	while (map_dupp[i])
+	{
+		j = 0;
+		while (map_dupp[i][j])
+		{
+			if (map_dupp[i][j] == 'E' || map_dupp[i][j] == 'C')
+				return (free_map(map_dupp), 0);
+			j++;
+		}
+		i++;
+	}
+	free_map(map_dupp);
+	return (1);
 }
