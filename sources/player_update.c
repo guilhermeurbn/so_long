ee6c81ea529a97f0973da9b913651623a96a83d5
Guilherme Urbano <guilhermeurbano@MacBook-Pro-de-Guilherme.local>
2025-09-22 15:25:46 +0100

    pronto

--- sources/player_update.c
+++ sources/player_update.c
@@ -6,7 +6,7 @@
 /*   By: guisanto <guisanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 18:51:06 by igvaz-fe          #+#    #+#             */
-/*   Updated: 2025/09/22 13:48:03 by guisanto         ###   ########.fr       */
+/*   Updated: 2025/09/22 14:00:39 by guisanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,8 @@ static void	player_update_image(char key, t_game *game)
 				"images/iron_man1.xpm", &game->img_w, &game->img_h);
 }
 
-void	player_move(t_game *game, char key)
+static int	player_reached_exit(t_game *game, char key)
 {
-	player_update_image(key, game);
 	if (game->map[game->y_player][game->x_player] == 'E' && game->n_colect == 0)
 	{
 		if (key == 'w')
@@ -45,9 +44,15 @@ void	player_move(t_game *game, char key)
 		game->moves++;
 		game->endgame = 1;
 		exit_game(game);
-		map_draw(game);
+		map_draw(game);	
+		return (1);
 	}
-	else if (game->map[game->y_player][game->x_player] == '1')
+	return (0);
+}
+
+static int	player_hit_wall(t_game *game, char key)
+{
+	if (game->map[game->y_player][game->x_player] == '1')
 	{
 		if (key == 'w')
 			game->y_player += 1;
@@ -57,7 +62,25 @@ void	player_move(t_game *game, char key)
 			game->x_player -= 1;
 		else if (key == 'a')
 			game->x_player += 1;
+		return (1);
 	}
+	return (0);
+}
+
+static int	player_exit(t_game *game, char key)
+{
+	if (player_reached_exit(game, key))
+		return (1);
+	if (player_hit_wall(game, key))
+		return (1);
+	return (0);
+}
+
+void	player_move(t_game *game, char key)
+{
+	player_update_image(key, game);
+	if (player_exit(game, key))
+		return ;
 	else
 	{
 		if (game->map[game->y_player][game->x_player] == 'C')
